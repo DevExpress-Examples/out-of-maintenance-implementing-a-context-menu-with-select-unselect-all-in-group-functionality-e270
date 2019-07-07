aa4c7a7a62824e1fd808bb63d7d56bab52d80cdd
khlopova.ekaterina <ekaterina.khlopova@devexpress.com>
2019-07-07 13:46:49 +0300

    example is updated to use the built-in Context Menu

--- CS/WebSite_Sample/Default.aspx.cs
+++ CS/WebSite_Sample/Default.aspx.cs
@@ -1,30 +1,38 @@
-#region Using
-using System;
-using System.Data;
-using System.Configuration;
-using System.Web;
-using System.Web.Security;
-using System.Web.UI;
-using System.Web.UI.WebControls;
-using System.Web.UI.WebControls.WebParts;
-using System.Web.UI.HtmlControls;
-#endregion
+using DevExpress.Web;
 
 public partial class _Default : System.Web.UI.Page {
 
-	protected void grid_CustomCallback(object sender, DevExpress.Web.ASPxGridView.ASPxGridViewCustomCallbackEventArgs e) {
-		string[] data = e.Parameters.Split('|');
-		if(data.Length == 2)
-			SetSelectionCore(int.Parse(data[1]), data[0] == "sel");
-	}
+    protected void grid_CustomCallback(object sender, ASPxGridViewCustomCallbackEventArgs e) {
+        string[] data = e.Parameters.Split('|');
+        if (data.Length == 2)
+            SetSelectionCore(int.Parse(data[1]), data[0] == "SelectAll");
+    }
 
-	void SetSelectionCore(int index, bool value) {
-		int startLevel = grid.GetRowLevel(index);
-		int count = grid.VisibleRowCount;
-		for(int i = 1 + index; i < count; i++) {
-			grid.Selection.SetSelection(i, value);
-			if(grid.GetRowLevel(i) <= startLevel)
-				break;
-		}
-	}
+    void SetSelectionCore(int visibleIndex, bool selected) {
+        int count = grid.GetChildRowCount(visibleIndex);
+        for (int i = 0; i < count; i++) {
+            object key = grid.GetChildRowValues(visibleIndex, i, "OrderID", "ProductID");
+            grid.Selection.SetSelectionByKey(key, selected);
+        }
+    }
+
+    protected void grid_FillContextMenuItems(object sender, ASPxGridViewContextMenuEventArgs e) {
+        if (e.MenuType == GridViewContextMenuType.Rows) {
+            e.Items.Add("Select All", "SelectAll");
+            e.Items.Add("Unselect All", "UnselectAll");
+        }
+    }
+
+    protected void grid_ContextMenuItemVisibility(object sender, ASPxGridViewContextMenuItemVisibilityEventArgs e) {
+        if (e.MenuType == GridViewContextMenuType.Rows) {
+            GridViewContextMenuItem selectAllItem = e.Items.FindByName("SelectAll");
+            GridViewContextMenuItem unSelectAllItem = e.Items.FindByName("UnselectAll");
+            for (int i = 0; i < grid.VisibleRowCount; i++) {
+                if (!grid.IsGroupRow(i)) {
+                    e.SetVisible(selectAllItem, i, false);
+                    e.SetVisible(unSelectAllItem,i, false);
+                }
+            }
+        }
+    }
 }
